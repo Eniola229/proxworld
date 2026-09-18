2b07370254de9db071caf3f665e1cc714f572503
Adeyemi Joshua <150144018+Eniola229@users.noreply.github.com>
2026-09-18 13:42:25 +0100

    Add Google Analytics tracking script

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -287,6 +287,15 @@
             white-space: nowrap;
         }
     </style>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-2T8TWXC01E"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+    
+      gtag('config', 'G-2T8TWXC01E');
+    </script>
 </head>
 <body>
 
